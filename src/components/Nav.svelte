265179cc2e40a6174671746e9a4b49ebbf05c4c1
Viet Hung Nguyen <vhn@sfu.ca>
2022-08-17 07:55:13 -0700

    Finish search bar

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -1,6 +1,8 @@
-<nav class="flex items-center justify-between p-2 bg-transparent w-full bg-black bg-opacity-20">
-  <span class="text-4xl text-bold">Meteo</span>
+<nav class="flex items-center justify-between p-2 w-full bg-black bg-opacity-20">
+  <span class="text-4xl font-bold">Meteo</span>
   <div class="flex items-center space-x-2">
-    <i class="fa-brands fa-github h-[2rem]"></i>
+    <button class="p-2 flex justify-center h-full hover:bg-black hover:bg-opacity-20 rounded-md">
+      <i class="fa-brands fa-github h-6 aspect-square"></i>
+    </button>
   </div>
 </nav>
--- src/components/Search.svelte
+++ src/components/Search.svelte
@@ -1,11 +1,27 @@
-<div class="relative flex items-center">
-  <i class="fa-solid fa-magnifying-glass absolute left-3 w-5 h-5" />
-  <input
-    type="name"
-    class="border-2 bg-transparent text-2xl px-10 py-3 rounded-md"
-    placeholder="Search for a city..."
-    autocomplete="off"
-    aria-label="Search city"
-  />
-  <i class="fa-solid fa-location-arrow w-6 h-6 absolute right-3" />
+<script>
+  import { isDark } from '../util/store';
+  $: ringColor = $isDark ? 'ring-white' : 'ring-black';
+  $: placeHolder = $isDark
+    ? 'placeholder:text-white'
+    : 'placeholder:text-black';
+</script>
+
+<div class="flex items-center w-full px-5 max-w-xl">
+  <div
+    class="relative flex flex-1 items-center opacity-60 focus-within:opacity-100"
+  >
+    <i class="fa-solid fa-magnifying-glass absolute pl-3 w-5 h-5" />
+    <input
+      type="name"
+      class="border-none ring-2 {ringColor} {placeHolder} placeholder:text-opacity-50 border-opacity-5 bg-transparent text-2xl pl-10 pr-3 py-3 rounded-md outline-0 w-full"
+      placeholder="Search for a city..."
+      autocomplete="off"
+      aria-label="Search city"
+    />
+  </div>
+  <button
+    class="ml-2 p-2 flex items-center justify-center hover:bg-black hover:bg-opacity-20 rounded-full"
+  >
+    <i class="fa-solid fa-location-arrow h-8 w-8" />
+  </button>
 </div>
