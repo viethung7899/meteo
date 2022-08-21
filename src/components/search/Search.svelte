f08f812eb69d531aa183b6d1e3c61e45986ebf74
Viet Hung Nguyen <vhn@sfu.ca>
2022-08-21 08:43:50 -0700

    Fox input border

--- src/components/search/Search.svelte
+++ src/components/search/Search.svelte
@@ -3,7 +3,7 @@
   import NavigationButton from './NavigationButton.svelte';
   import Suggestion from './Suggestion.svelte';
 
-  $: ringColor = $isDark ? 'ring-white' : 'ring-black';
+  $: border = $isDark ? 'border-white' : 'border-black';
   $: placeHolder = $isDark
     ? 'placeholder:text-white'
     : 'placeholder:text-black';
@@ -23,7 +23,7 @@
     <i class="fa-solid fa-magnifying-glass absolute pl-3 w-5 h-5" />
     <input
       type="name"
-      class="border-none ring-2 {ringColor} {placeHolder} placeholder:text-opacity-50 border-opacity-5 bg-transparent text-2xl pl-10 pr-3 py-3 rounded-md outline-0 w-full"
+      class="border-2 {border} {placeHolder} placeholder:text-opacity-50 bg-transparent text-2xl pl-10 pr-3 py-3 rounded-md outline-0 w-full"
       placeholder="Search for a city..."
       autocomplete="off"
       aria-label="Search city"
